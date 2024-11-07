234b7c2ea3c69c447cb98c769d0b7fd3af67e167
oswal99 <118828910+oswal99@users.noreply.github.com>
2024-11-07 13:12:47 +0000

    Something
    a

---
+++ Assets/Oswald/PlayButton_Control.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayButton_Control : MonoBehaviour
+{
+    
+
+
+    public void Play()
+    {
+        Application.LoadLevel("SampleScene");
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
