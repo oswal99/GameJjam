6ac7c41645a383846dd63962b846c4ed7ba19edf
oswal99 <118828910+oswal99@users.noreply.github.com>
2024-11-07 12:39:22 +0000

    a

---
+++ Assets/Box_Control.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box_Control : MonoBehaviour
+{
+    public GameObject prefab;
+    List<GameObject> gameObject;
+    
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+    }
+    
+
+
+
+}
