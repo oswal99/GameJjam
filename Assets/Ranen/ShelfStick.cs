df8085384996e8c750d7d2bea52efb4d319373f8
oswal99 <118828910+oswal99@users.noreply.github.com>
2024-11-08 15:31:55 +0000

    A

--- Assets/Ranen/ShelfStick.cs
+++ Assets/Ranen/ShelfStick.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class ShelfStick : MonoBehaviour
 {
+    private int count;
+
+    public void Start()
+    {
+        count = 0;
+       
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Item")
         {
             collision.transform.SetParent(transform, false);
-            collision.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;  
+            collision.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+           count =  count + 1;
+            CountList();
         }
     }
 
+    private void CountList()
+    {
+        if(count >= 12)
+        {
+            Application.LoadLevel("Win");
+        }
+    }
     
 }
